665db5a0f79d0a37c388338a85f938f857d6c44f
kwe <kwevans@gmail.com>
2022-10-17 09:45:26 +0100

    Add Constructor

--- StillWaiting/RepeatingService.cs
+++ StillWaiting/RepeatingService.cs
@@ -4,6 +4,11 @@ public class RepeatingService: BackgroundService
 {
     private readonly PeriodicTimer _timer = new(TimeSpan.FromMinutes(4));
     
+    // create a constructor that takes in the IServiceProvider
+    public RepeatingService()
+    {
+        Console.WriteLine("RepeatingService constructor");
+    }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (await _timer.WaitForNextTickAsync(stoppingToken) && !stoppingToken.IsCancellationRequested)
