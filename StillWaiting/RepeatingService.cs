0992e400bce758131cfb53d34557e362b2afd365
kwe <kwevans@gmail.com>
2022-10-27 08:42:02 +0100

    call posts periodically works!

--- StillWaiting/RepeatingService.cs
+++ StillWaiting/RepeatingService.cs
@@ -1,13 +1,17 @@
+using StillWaiting.Clients;
+
 namespace StillWaiting;
 
 public class RepeatingService: BackgroundService
 {
     private readonly PeriodicTimer _timer = new(TimeSpan.FromMinutes(1));
     private readonly IMyDependency _myDependency;
+    private readonly IPostsClient _postsClient;
 
-    public RepeatingService(IMyDependency myDependency)
+    public RepeatingService(IMyDependency myDependency, IPostsClient postsClient)
     {
         _myDependency = myDependency;
+        _postsClient = postsClient;
         Console.WriteLine("RepeatingService constructor");
     }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -15,6 +19,7 @@ public class RepeatingService: BackgroundService
         while (await _timer.WaitForNextTickAsync(stoppingToken) && !stoppingToken.IsCancellationRequested)
         {
             _myDependency.WriteMessage(DateTime.Now.ToString("O"));
+            var posts = _postsClient.GetPostsAsync();
         }
     }
 }
